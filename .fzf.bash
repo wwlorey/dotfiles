4b8e8bb87d765c7dad95625defa9c1b0ffd9f0e0
William Lorey <will@williamlorey.com>
2022-07-07 17:59:20 -0700

    Add fzf config for Intel Macs

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,30 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+if [[ $(uname -p) == 'i386' ]]; then
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+        export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/usr/local/opt/fzf/shell/key-bindings.bash"
+else
+    # Setup fzf
+    # ---------
+    if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+        export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+    fi
+
+    # Auto-completion
+    # ---------------
+    [[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+
+    # Key bindings
+    # ------------
+    source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
