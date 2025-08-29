b524994bf471b92ade658c8ffbe99b4429dc095d
William Lorey <william@lorey.co>
2025-08-29 16:31:00 -0500

    Fix excessive booping in screen dim script

--- .hammerspoon/docked.lua
+++ .hammerspoon/docked.lua
@@ -1,31 +1,26 @@
-function dimMacBookScreenWhenExternalMonitorDetected()
+function dimLaptopScreenWhenExternalMonitorDetected()
     local screens = hs.screen.allScreens()
-
-    if #screens > 1 then
-        hs.osascript.applescript([[
-            tell application "System Events"
-                repeat 20 times
-                    key code 107
-                end repeat
-            end tell
-        ]])
-    else
-        -- First dim all the way down, then bring back up to ~50%
-        hs.osascript.applescript([[
-            tell application "System Events"
-                repeat 20 times
-                    key code 107  -- brightness down
-                end repeat
-                repeat 10 times
-                    key code 113  -- brightness up
-                end repeat
-            end tell
-        ]])
+    local laptopScreen = nil
+    
+    -- Find the built-in display
+    for _, screen in ipairs(screens) do
+        if screen:name():find("Built%-in") or screen:name():find("Color LCD") then
+            laptopScreen = screen
+            break
+        end
+    end
+    
+    if laptopScreen then
+        if #screens > 1 then
+            laptopScreen:setBrightness(0)
+        else
+            laptopScreen:setBrightness(0.5)
+        end
     end
 end
 
-screenWatcher = hs.screen.watcher.new(dimMacBookScreenWhenExternalMonitorDetected)
+screenWatcher = hs.screen.watcher.new(dimLaptopScreenWhenExternalMonitorDetected)
 screenWatcher:start()
 
 -- Run once on startup
-dimMacBookScreenWhenExternalMonitorDetected()
+dimLaptopScreenWhenExternalMonitorDetected()
